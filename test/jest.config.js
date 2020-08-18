da0e00e712c7e6e6971c5ccc5f9e141ad7339c58
rafael <faeh777@gmail.com>
2020-08-18 09:39:56 -0300

    Fix de the problema Cannot read property addAliases of undefined

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,6 @@
 const { resolve } = require('path');
 const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
-
 module.exports = {
     ...rootConfig, ...{
         rootDir: root,
